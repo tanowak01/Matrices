d339167fa5a3d9218a96ff95aa3ecbff0c1a746d
tanowak01 <tnowak4@wisc.edu>
2022-02-01 12:42:59 -0600

    R

--- Matrix.cpp
+++ Matrix.cpp
@@ -237,8 +237,8 @@ Matrix Matrix::rref() {
 			//need to subtract from lower rows
 			//
 			for(int lower = i+1; lower < rows; lower++){
-				cout << values[i][i] << endl;
-				cout << values[lower][i] << endl;
+				cout << "ROW1: " << values[i][i] << endl;
+				cout << "ROW2: " << values[lower][i] << endl;
 				double val = values[i][i]/values[lower][i];
 				cout << val << endl;
 				//rowOperations(i, lower, val);
