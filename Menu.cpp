6d440b643403cc808d5483b909f9fc0aee4c9757
tanowak01 <tnowak4@wisc.edu>
2022-02-09 16:32:47 -0600

    here

--- Menu.cpp
+++ Menu.cpp
@@ -9,8 +9,13 @@ void Menu::createNewMatrix(){
     cout << "What is the name of this matrix?" << endl;
     string name;
     cin >> name;
+    while(matMap.find(name) == matMap.end()){
+        cout << "That name is being used, choose another: " << endl;
+        cin >> name;
+    }
     Matrix* m1 = new Matrix(0,0);
     cin >> *m1;
-    cout << matMap.insert({name, m1}).second;
-    cout << matMap.insert({name, m1}).second;
+    if(!matMap.insert({name, m1}).second){
+        cout << "Insert failed, please try again" << endl;
+    }
 }
