a937b5416ce478a7d0679160bf6d2e01499bd50f
tanowak01 <tnowak4@wisc.edu>
2022-02-09 16:00:56 -0600

    Working on menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,9 +1,18 @@
 #include "Matrix.h"
+#include <map>
 
 using namespace std;
 
 int main(){
-    Matrix mat(identity(4));
-    cout << mat;
+    map<string, Matrix> matrices;
+    cout << "Welcome to the Calculator App!" << endl;
+    cout << "Please enter in a matrix" << endl;
+    Matrix* m1 = new Matrix(1,1);
+    cin >> *m1;
+    cout << *m1;
     return 0;
+}
+
+int menuOptions(){
+
 }
